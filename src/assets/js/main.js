d9975de94891c9169f6f8c083c0cd193147a957f
Pinchas David Genger <pgenger@fabuwood.com>
2018-01-29 13:13:31 -0500

    small fixes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,7 @@
 require("jquery");
 require("turbolinks");
-require("./libraries/owl.carousel.js");
-import slick from "slick-carousel";
-require("slick-carousel/slick/slick.scss");
-require("slick-carousel/slick/slick-theme.scss");
 require("jquery-modal/jquery.modal.min.js");
 require("jquery-modal/jquery.modal.min.css");
-require("./libraries/jquery.lettering.js");
-require("./libraries/modernizr-custom-es6Number.js");
 Turbolinks.start();
 
 $(document).on("turbolinks:load", function() {
@@ -21,6 +15,5 @@ $(document).on("turbolinks:load", function() {
 });
 
 require("./libraries/sticky-header.js");
-require("./quality-report.js");
 
 require("../scss/style.scss");
